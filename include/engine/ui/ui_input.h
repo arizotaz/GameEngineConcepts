5f8e798915e59fba6fc8df5ee5adbf0af55aaed9
Colton Staiduhar <colton@staiduhar.com>
2026-04-16 12:39:46 -0400

    Checkbox Element

--- include/engine/ui/ui_input.h
+++ include/engine/ui/ui_input.h
@@ -85,11 +85,20 @@ namespace UI {
                 this->text = text;
                 this->last_value = this->text;
             }
+            void SetValueAsUser(std::string text)
+            {
+                this->text = text;
+                this->last_value = "";
+                this->changed = true;
+                this->selected = false;
+                this->last_state = true;
+            }
 
             /**
              * If the text in the field change when the element is no longer selected
              */
-            bool Changed() const {
+            bool Changed() const
+            {
                 return changed;
             }
 
@@ -97,6 +106,84 @@ namespace UI {
             bool selected = false, last_state = false;
             std::string last_value = "";
             bool changed = false;
+        };
+
+        class Checkbox : public Button {
+        public:
+            Checkbox()
+                : checkboxOffset(0, 0)
+            {
+                tr = new GEC::TextRender::Text();
+            }
+            ~Checkbox()
+            {
+                delete tr;
+            };
+
+            virtual void Interact() override
+            {
+                float lastX = x;
+                float lastY = y;
+                float lastWidth = width;
+                float lastHeight = height;
+
+                x = x + checkboxOffset.First();
+                y = y + checkboxOffset.Second();
+                width = height;
+                height = height;
+
+                Button::Interact();
+
+                x = lastX;
+                y = lastY;
+                width = lastWidth;
+                height = lastHeight;
+
+                if (Clicked())
+                    enabled = !enabled;
+
+                changed = false;
+                if (lastState != enabled)
+                    changed = true;
+                lastState = enabled;
+            }
+            virtual void Render() override
+            {
+
+                float fontSize = height / 1.5f;
+                float buttonSize = height;
+                tr->SetText(this->text);
+                tr->Align(0, 1);
+
+                GEC::Vector2<float, float> tsize = tr->GetSize(fontSize) / fontSize;
+
+                float nSize = tsize.First() + 4 + buttonSize;
+
+                checkboxOffset.Set(buttonSize / 2, 0);
+
+                GEC::Render::SetColor(150);
+                GEC::Render::Rect(x + checkboxOffset.First(), y + checkboxOffset.Second(), buttonSize, buttonSize);
+                GEC::Render::SetColor(80);
+                GEC::Render::Rect(x + checkboxOffset.First(), y + checkboxOffset.Second(), buttonSize - 4, buttonSize - 4);
+                if (enabled) {
+                    GEC::Render::SetColor(255);
+                    GEC::Render::Rect(x + checkboxOffset.First(), y + checkboxOffset.Second(), buttonSize - 6, buttonSize - 6);
+                }
+                GEC::Render::SetColor((int)0);
+                tr->Render(x + buttonSize + 4, y, 10.1, fontSize);
+            }
+
+            bool GetValue() const { return enabled; }
+            void SetValue(bool val) { this->enabled = val; }
+            bool Changed() const
+            {
+                return changed;
+            }
+
+        private:
+            bool enabled, lastState, changed;
+            GEC::TextRender::Text* tr;
+            Vector2<float, float> checkboxOffset;
         };
 
     }
